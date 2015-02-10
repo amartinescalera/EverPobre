378d0117bcc4162a17260af665adae03e61028f7
Antonio Martin <antoniomeh@gmail.com>
2015-02-10 19:57:30 +0000

    Initial Commit

---
+++ EverPobre/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EverPobre
+//
+//  Created by Antonio Martin on 10/02/2015.
+//  Copyright (c) 2015 antoniomeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
